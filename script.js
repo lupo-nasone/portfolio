22c6396c207b5b81304522199e9fc1288b45c95c
lupo-nasone <matteo.faginali@gmail.com>
2022-05-06 00:33:04 +0200

    new bg

--- script.js
+++ script.js
@@ -3,25 +3,3 @@ document.onkeyup = function (e) {
     window.location.href = "segretume/pupucaca.html";
   }
 };
-// prettier
-var notte = false;
-
-function darker() {
-  if (notte == false) {
-    document.getElementById('quarta').src = './backgrounddark/foto1.png';
-    document.getElementById('terza').src = './backgrounddark/foto2.png';
-    document.getElementById('seconda').src = './backgrounddark/foto3.png';
-    document.getElementById('prima').src = './backgrounddark/foto4.png';
-    document.getElementsByTagName('body')[0].style.backgroundColor = 'rgb(0,0,70)';
-    notte = true;
-  }
-  else {
-    document.getElementById('quarta').src = 'background/layer_4.png';
-    document.getElementById('terza').src = 'background/layer_3.png';
-    document.getElementById('seconda').src = 'background/layer_2.png';
-    document.getElementById('prima').src = 'background/layer_1.png';
-    document.getElementsByTagName("body")[0].style.backgroundColor = "#3193e2";
-    notte = false;
-  }
-  
-}
